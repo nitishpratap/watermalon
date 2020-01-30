6fb1b94affc16e0d8fa016870dd6d543dc47d51e
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-01-30 17:12:40 +0530

    Add files via upload

---
+++ watermelon/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int weight;
+    scanf("%d",&weight);
+    if(weight%2==0)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
